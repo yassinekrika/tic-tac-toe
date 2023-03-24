5506f9b3cef87aef658b8a0aec293cf6cf6db718
YassG4mer <yassinekrika2016@gmail.com>
2023-03-24 01:54:19 +0100

    first commit

---
+++ src/components/Square.js
@@ -0,0 +1,18 @@
+
+const Square = ({ value }) => {
+
+    const handleClick = () => {
+        console.log('clicked')
+    }
+
+    return (
+        <button 
+            className="square"
+            onClick={handleClick}
+        >
+            {value}
+        </button>
+    )
+}
+
+export default Square
